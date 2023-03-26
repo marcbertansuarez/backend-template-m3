cf8e6fe26ad658248f87ae4947c38af587a73495
Marc Bertran <mbertransuarez@gmail.com>
2023-03-26 19:09:44 +0200

    seed done

--- seed/index.js
+++ seed/index.js
@@ -1,7 +1,25 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 mongoose.set('strictQuery', true);
+const User = require('../models/User');
 const LineUp = require('../models/LineUp');
+const saltRounds = 10;
+
+const users = [
+  {
+    email: 'user1@example.com',
+    password: 'user1Password',
+    username: 'user1',
+    role: 'user'
+  },
+  {
+    email: 'user2@example.com',
+    password: 'user2Password',
+    username: 'user2',
+    role: 'user'
+  }
+];
 
 const lineups = [
   {
@@ -10,15 +28,41 @@ const lineups = [
     map: 'Bind',
     description: 'Best lineup of Brimstone',
     video: 'https://www.youtube.com/watch?v=txjSTa4eXQI&ab_channel=RikuZalaValorant'
-  }
+  },
+  {
+    title: 'Astra lineup',
+    agent: 'Astra',
+    map: 'Ascent',
+    description: 'Best lineup of Astra',
+    video: 'https://www.youtube.com/watch?v=txjSTa4eXQI&ab_channel=RikuZalaValorant'
+  } 
 ]
 
-// Place the array you want to seed
+async function hashUserPasswords() {
+  return Promise.all(
+    users.map(async user => {
+      const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+      return { ...user, hashedPassword };
+    })
+  );
+}
+
 
 mongoose.connect(process.env.MONGO_URL)
-  .then(x => console.log(`Connected to ${x.connection.name}`))
-  .then(() => {
-    return // Model.create(array)
+  .then(async x => {
+    console.log(`Connected to ${x.connection.name}`);
+
+    await User.deleteMany();
+    await LineUp.deleteMany();
+    const hashedUsers = await hashUserPasswords();
+    const createdUsers = await User.create(hashedUsers); 
+
+    const lineupsWithAuthor = lineups.map((lineup, index) => ({
+      ...lineup,
+      author: createdUsers[index]._id,
+    }));
+
+    return LineUp.create(lineupsWithAuthor);
   })
   .then(() => {
     console.log('Seed done 🌱');
