f769fb310e9fc36e6ce4c43bcc36fd8d55f10a44
Marc Bertran <mbertransuarez@gmail.com>
2023-04-05 12:07:12 +0200

    liked profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const LineUp = require('../models/LineUp');
 const User = require('../models/User');
+const Like = require('../models/Like');
 const { isAuthenticated, isAdmin } = require('../middlewares/jwt');
 const getLikes = require('../utils/likesHelper');
 
@@ -35,7 +36,7 @@ router.put('/edit', isAuthenticated, async (req, res, next) => {
     const userId = req.payload._id;
     const { username, image } = req.body; 
     try {
-        const userDB = await User.findById(user);
+        const userDB = await User.findById(userId);
         if(userDB._id.toString() !== userId) {
             res.status(403).json({message: 'You are not allowed to edit this profile'});
         } else {
@@ -46,5 +47,26 @@ router.put('/edit', isAuthenticated, async (req, res, next) => {
         console.log(error)
     }
 });
+
+router.get('/liked', isAuthenticated, async (req, res, next) => {
+    const userId = req.payload._id;
+    try {
+        const userDB = await User.findById(userId);
+        const liked = await Like.find({userId: userId}).populate('lineupId');
+        const prePromiseLikes = JSON.parse(JSON.stringify(liked));
+        const likedLineUps = await Promise.all(prePromiseLikes.map(async (lineup) => {
+            return await getLikes(lineup.lineupId, userDB);
+        }))
+        res.status(200).json({
+            user: {
+                username: userDB.username,
+                image: userDB.image
+            },
+            likedLineUps
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 module.exports = router;
